dfb2cd7af3e35c8d495074d6816d101c280cb9c6
Josefina Pineda <jossepincor@gmail.com>
2024-07-19 23:38:38 -0600

    Update: vite.config.js

--- js/items.js
+++ js/items.js
@@ -28,64 +28,64 @@ function loadStorageSampleData() {
     if (!localStorage.getItem("items")) {
         const sampleItems = [
             {
-                name: "Body Damon",
-                imageUrl: "/img/Body-Damon.jpg",
-                hoverImageUrl: "/img/Body-Damon(2).jpg",
-                price: "799.99"
+                name: 'Body Damon',
+                imageUrl: '/img/Body-Damon.jpg',
+                hoverImageUrl: '/img/Body-Damon(2).jpg',
+                price: 799.99
             },
             {
-                name: "Body Carmina",
-                imageUrl: "/img/Body-Carmina.jpg",
-                hoverImageUrl: "/img/Body-Carmina(2).jpg",
-                price: "799.99"
+                name: 'Body Carmina',
+                imageUrl: '/img/Body-Carmina.jpg',
+                hoverImageUrl: '/img/Body-Carmina(2).jpg',
+                price: 799.99
             },
             {
-                name: "Bustier Ale",
-                imageUrl: "/img/Bustier-Ale.jpg",
-                hoverImageUrl: "/img/Bustier-Ale(2).jpg",
-                price: "799.99"
+                name: 'Bustier Ale',
+                imageUrl: '/img/Bustier-Ale.jpg',
+                hoverImageUrl: '/img/Bustier-Ale(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Angel",
-                imageUrl: "/img/Set-Angel.jpg",
-                hoverImageUrl: "/img/Set-Angel(2).jpg",
-                price: "799.99"
+                name: 'Set Angel',
+                imageUrl: '/img/Set-Angel.jpg',
+                hoverImageUrl: '/img/Set-Angel(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Daniela",
-                imageUrl: "/img/Set-Daniela.jpg",
-                hoverImageUrl: "/img/Set-Daniela(2).jpg",
-                price: "799.99"
+                name: 'Set Daniela',
+                imageUrl: '/img/Set-Daniela.jpg',
+                hoverImageUrl: '/img/Set-Daniela(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Cordelia",
-                imageUrl: "/img/Set-Cordelia.jpg",
-                hoverImageUrl: "/img/Set-Cordelia(2).jpg",
-                price: "799.99"
+                name: 'Set Cordelia',
+                imageUrl: '/img/Set-Cordelia.jpg',
+                hoverImageUrl: '/img/Set-Cordelia(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Vedette",
-                imageUrl: "/img/Set-Vedette.jpg",
-                hoverImageUrl: "/img/Set-Vedette(2).jpg",
-                price: "799.99"
+                name: 'Set Vedette',
+                imageUrl: '/img/Set-Vedette.jpg',
+                hoverImageUrl: '/img/Set-Vedette(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Catalina",
-                imageUrl: "/img/Set-Catalina.jpg",
-                hoverImageUrl: "/img/Set-Catalina(2).jpg",
-                price: "799.99"
+                name: 'Set Catalina',
+                imageUrl: '/img/Set-Catalina.jpg',
+                hoverImageUrl: '/img/Set-Catalina(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Florentina",
-                imageUrl: "/img/Set-Florentina.jpg",
-                hoverImageUrl: "/img/Set-Florentina(2).jpg",
-                price: "799.99"
+                name: 'Set Florentina',
+                imageUrl: '/img/Set-Florentina.jpg',
+                hoverImageUrl: '/img/Set-Florentina(2).jpg',
+                price: 799.99
             },
             {
-                name: "Set Susan",
-                imageUrl: "/img/Set-Susan.jpg",
-                hoverImageUrl: "/img/Set-Susan(2).jpg",
-                price: "799.99"
+                name: 'Set Susan',
+                imageUrl: '/img/Set-Susan.jpg',
+                hoverImageUrl: '/img/Set-Susan(2).jpg',
+                price: 799.99
             },
 
         ];
--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
         login: resolve(__dirname, 'src/pages/login/login.html'),
         contact: resolve(__dirname, 'src/pages/contacto/contacto.html'),
         object: resolve(__dirname, 'src/pages/adminProductos/objetos.html'),
+        cart: resolve(__dirname, 'src/pages/cart/cart.html'),
+        checkout: resolve(__dirname, 'src/pages/cart/checkout.html')
       },
       external: [
         'js/itemsController.js', // Sustituye 'nombre-del-modulo-problematico' con el nombre real del módulo

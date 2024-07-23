6ca05d0bd3ba44362bcafdc948c2c45a51999aa2
estefaniaoc <ortizc.estefania@gmail.com>
2024-07-22 19:43:53 -0600

    Change: ruta de contacto.js en html

--- src/pages/contacto/contacto.js
+++ src/pages/contacto/contacto.js
@@ -110,7 +110,8 @@ function postContactForm() {
     };
 
     (function() {
-        emailjs.init("eToDepjFcMp--P6CW"); 
+        emailjs.init("eToDepjFcMp--P6CW"); // Reemplaza "YOUR_PUBLIC_KEY" con la Public Key que copiaste
+    })();
     
 
     emailjs.send("service_uzjz2mc","template_nlhxwj7" ,templateParams)
